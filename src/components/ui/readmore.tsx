62e5c2ee00d3b8a2b4062bc930bac50c078aac34
Rizky Hady <reazon7@gmail.com>
2024-06-10 10:43:25 +0700

    custom readmore

--- src/components/ui/readmore.tsx
+++ src/components/ui/readmore.tsx
@@ -1,27 +1,93 @@
-import cn from '@/utils/class-names'
-import { ReadMoreWeb, ReadMoreWebProps } from 'react-shorten'
+'use client'
 
-export type ReadMoreProps = Omit<ReadMoreWebProps, 'truncate'> & {
+import cn from '@/utils/class-names'
+import { useCallback, useState } from 'react'
+import {
+  ReadMore as RsReadMore,
+  ReadMoreWebProps as RsReadMoreWebProps,
+} from 'react-shorten'
+
+export type ReadMoreProps = Omit<RsReadMoreWebProps, 'truncate'> & {
   truncate?: number
 }
 
-export default function ReadMore({
+export const ReadMore: React.FC<ReadMoreProps> = ({
   truncate = 100,
   showMoreText = 'Lihat Selengkapnya',
-  showLessText = '',
+  showLessText,
   className,
-  ...props
-}: ReadMoreProps) {
+  style,
+  children,
+}) => {
+  const [expanded, setExpanded] = useState(false)
+  const onShowMore = useCallback(() => setExpanded(true), [])
+  const onShowLess = useCallback(() => setExpanded(false), [])
+
   return (
-    <ReadMoreWeb
+    <RsReadMore
       truncate={truncate}
-      showMoreText={showMoreText}
-      showLessText={showLessText}
-      className={cn(
-        'text-primary font-medium hover:text-primary-dark',
-        className
-      )}
-      {...props}
-    />
+      expanded={expanded}
+      showMore={
+        <>
+          {'... '}
+          <button
+            type="button"
+            onClick={onShowMore}
+            className={cn(
+              'text-primary font-medium hover:text-primary-dark',
+              className
+            )}
+            style={style}
+          >
+            {showMoreText}
+          </button>
+        </>
+      }
+      showLess={
+        <>
+          {showLessText && (
+            <>
+              {' '}
+              <button
+                type="button"
+                onClick={onShowLess}
+                className={cn(
+                  'text-primary font-medium hover:text-primary-dark',
+                  className
+                )}
+                style={style}
+              >
+                {showLessText}
+              </button>
+            </>
+          )}
+        </>
+      }
+    >
+      {children}
+    </RsReadMore>
   )
 }
+
+export default ReadMore
+
+// export default function ReadMore({
+//   truncate = 100,
+//   showMoreText = 'Lihat Selengkapnya',
+//   showLessText = '',
+//   className,
+//   ...props
+// }: ReadMoreProps) {
+//   return (
+//     <RSReadMore
+//       truncate={truncate}
+//       showMoreText={showMoreText}
+//       showLessText={showLessText}
+//       className={cn(
+//         'text-primary font-medium hover:text-primary-dark',
+//         className
+//       )}
+//       {...props}
+//     />
+//   )
+// }
