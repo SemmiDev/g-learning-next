4ad21fd1c0fcdee4e9616e050efb21fa5ac88b96
Rizky Hady <reazon7@gmail.com>
2024-06-14 08:35:36 +0700

    loader

--- src/app/(hydrogen)/layout.tsx
+++ src/app/(hydrogen)/layout.tsx
@@ -1,11 +1,23 @@
 'use client'
 
 import HydrogenLayout from '@/layouts/hydrogen/layout'
+import { useSession } from 'next-auth/react'
+import { RiLoader5Fill } from 'react-icons/ri'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
 export default function DefaultLayout({ children }: LayoutProps) {
+  const { status } = useSession()
+
+  if (status !== 'authenticated') {
+    return (
+      <div className="flex justify-center items-center h-svh">
+        <RiLoader5Fill size={40} className="text-primary animate-spin" />
+      </div>
+    )
+  }
+
   return <HydrogenLayout>{children}</HydrogenLayout>
 }
