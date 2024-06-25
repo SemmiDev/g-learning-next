811c78702b30907eb039a60db98b4457f7f44fa5
Rizky Hady <reazon7@gmail.com>
2024-06-25 13:56:52 +0700

    ganti icon loading

--- src/app/(hydrogen)/layout.tsx
+++ src/app/(hydrogen)/layout.tsx
@@ -2,7 +2,7 @@
 
 import HydrogenLayout from '@/layouts/hydrogen/layout'
 import { useSession } from 'next-auth/react'
-import { RiLoader5Fill } from 'react-icons/ri'
+import { CgSpinner } from 'react-icons/cg'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -14,7 +14,7 @@ export default function DefaultLayout({ children }: LayoutProps) {
   if (status !== 'authenticated') {
     return (
       <div className="flex justify-center items-center h-svh">
-        <RiLoader5Fill size={40} className="text-primary animate-spin" />
+        <CgSpinner size={40} className="text-primary animate-spin" />
       </div>
     )
   }
