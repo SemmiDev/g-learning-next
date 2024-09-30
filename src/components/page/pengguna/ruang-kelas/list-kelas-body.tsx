a55e9b666e76a461e4fc4a674bd97a4e710d5846
Rizky Hady <reazon7@gmail.com>
2024-09-30 16:05:32 +0700

    update

--- src/components/page/pengguna/ruang-kelas/list-kelas-body.tsx
+++ src/components/page/pengguna/ruang-kelas/list-kelas-body.tsx
@@ -2,7 +2,7 @@
 
 import { hapusKelasAction } from '@/actions/pengguna/ruang-kelas/hapus'
 import { listKelasAction } from '@/actions/pengguna/ruang-kelas/list'
-import { Button, ModalConfirm, Text, Title } from '@/components/ui'
+import { Button, Loader, ModalConfirm, Text, Title } from '@/components/ui'
 import { handleActionWithToast } from '@/utils/action'
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
@@ -70,37 +70,47 @@ export default function ListKelasBody() {
           </Text>
         </div>
         <Button size="sm" onClick={() => setShowModalBuatKelas(true)}>
-          Tambah Kelas
+          Buat Kelas
         </Button>
       </div>
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {list.map((item) => (
-          <KelasCard
-            key={item.kelas.id}
-            id={item.kelas.id}
-            program={item.kelas.nama_kelas}
-            kelas={item.kelas.sub_judul}
-            image={item.kelas.thumbnail ?? undefined}
-            tipe={item.kelas.tipe}
-            instansi={item.kelas.nama_instansi}
-            instansiCentang={true}
-            pengajar={item.nama_pemilik}
-            jumlahPeserta={item.total_peserta}
-            jadwal={
-              item.jadwal && item.jadwal.length > 0
-                ? `${
-                    item.jadwal[0].hari
-                  }, ${item.jadwal[0].waktu_mulai.substring(
-                    0,
-                    5
-                  )} - ${item.jadwal[0].waktu_sampai.substring(0, 5)}`
-                : '-'
-            }
-            onPengaturan={(id) => setIdPengaturan(id)}
-            onDelete={(id) => setIdHapus(id)}
-          />
-        ))}
-      </div>
+      {isLoading || (!list.length && isFetching) ? (
+        <Loader height={300} />
+      ) : list.length > 0 ? (
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {list.map((item) => (
+            <KelasCard
+              key={item.kelas.id}
+              id={item.kelas.id}
+              program={item.kelas.nama_kelas}
+              kelas={item.kelas.sub_judul}
+              image={item.kelas.thumbnail ?? undefined}
+              tipe={item.kelas.tipe}
+              instansi={item.kelas.nama_instansi}
+              instansiCentang={true}
+              pengajar={item.nama_pemilik}
+              jumlahPeserta={item.total_peserta}
+              jadwal={
+                item.jadwal && item.jadwal.length > 0
+                  ? `${
+                      item.jadwal[0].hari
+                    }, ${item.jadwal[0].waktu_mulai.substring(
+                      0,
+                      5
+                    )} - ${item.jadwal[0].waktu_sampai.substring(0, 5)}`
+                  : '-'
+              }
+              onPengaturan={(id) => setIdPengaturan(id)}
+              onDelete={(id) => setIdHapus(id)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-72">
+          <Text size="sm" weight="medium">
+            Belum ada kelas
+          </Text>
+        </div>
+      )}
 
       <BuatKelasModal
         showModal={showModalBuatKelas}
