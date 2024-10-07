e804503cc6232d03b9811babee9e767d9e4ef070
Rizky Hady <reazon7@gmail.com>
2024-10-07 10:19:07 +0700

    forwardref

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,22 +1,27 @@
+import { forwardRef } from 'react'
 import { Loader as RizLoader, LoaderTypes as RizLoaderTypes } from 'rizzui'
 
 export type LoaderProps = RizLoaderTypes & {
   height?: number
 }
 
-export default function Loader({
-  height,
-  variant = 'spinner',
-  color = 'primary',
-  size = 'lg',
-  ...props
-}: LoaderProps) {
+export default forwardRef<HTMLDivElement, LoaderProps>(function Loader(
+  {
+    height,
+    variant = 'spinner',
+    color = 'primary',
+    size = 'lg',
+    ...props
+  }: LoaderProps,
+  ref
+) {
   return (
     <div
+      ref={ref}
       className="flex justify-center items-center"
       style={height ? { height: `${height}px` } : {}}
     >
       <RizLoader variant={variant} color={color} size={size} {...props} />
     </div>
   )
-}
+})
