a8813058c781a115ac85b95e4e3536552786d2d0
Rizky Hady <reazon7@gmail.com>
2024-10-07 11:52:18 +0700

    update

--- src/app/(hydrogen)/tes/2/page.tsx
+++ src/app/(hydrogen)/tes/2/page.tsx
@@ -17,6 +17,7 @@ import {
   ControlledTextarea,
   ControlledUploadFile,
   Form,
+  KelasItemType,
   Materi,
   MateriItemType,
   PaketSoalItemType,
@@ -49,9 +50,9 @@ const formSchema = z.object({
   // tesRupiah: z.string().pipe(required),
   // tesSelect: z.any().superRefine(objectRequired),
   // tesAsyncSelect: z.any().superRefine(objectRequired),
-  tesMedia: z.array(z.any()).superRefine(arrayRequired),
-  tesMateri: z.any().superRefine(objectRequired),
-  tesSoal: z.any().superRefine(objectRequired),
+  // tesMedia: z.array(z.any()).superRefine(arrayRequired),
+  // tesMateri: z.any().superRefine(objectRequired),
+  // tesSoal: z.any().superRefine(objectRequired),
   tesKelas: z.any().superRefine(objectRequired),
   // tesDate: z.date(),
   // tesFiles: z.array(z.any()).superRefine(arrayRequired),
@@ -68,10 +69,10 @@ type FormSchema = {
   // tesRupiah?: number | string
   // tesSelect?: SelectOptionType
   // tesAsyncSelect?: SelectOptionType
-  tesMedia?: PustakaMediaFileType[]
-  tesMateri?: MateriItemType
-  tesSoal?: PaketSoalItemType
-  tesKelas?: PaketSoalItemType
+  // tesMedia?: PustakaMediaFileType[]
+  // tesMateri?: MateriItemType
+  // tesSoal?: PaketSoalItemType
+  tesKelas?: KelasItemType
   // tesDate?: Date
   // tesFiles?: UploadFileType[]
   // tesSwitch: boolean
@@ -165,25 +166,25 @@ export default function Tes2Page() {
             errors={errors}
             isClearable
           /> */}
-          <ControlledPustakaMedia
+          {/* <ControlledPustakaMedia
             name="tesMedia"
             control={control}
             label="Pilih Berkas"
             errors={errors}
             multiple
-          />
-          <ControlledMateri
+          /> */}
+          {/* <ControlledMateri
             name="tesMateri"
             control={control}
             label="Pilih Materi"
             errors={errors}
-          />
-          <ControlledPaketSoal
+          /> */}
+          {/* <ControlledPaketSoal
             name="tesSoal"
             control={control}
             label="Pilih Paket Soal"
             errors={errors}
-          />
+          /> */}
           <ControlledKelas
             name="tesKelas"
             control={control}
