6ce5e44d5383bc525194f2a131df2a10f29b9418
Rizky Hady <reazon7@gmail.com>
2024-10-09 11:56:31 +0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // reactStrictMode: false,
+    productionBrowserSourceMaps: false, // Disable source maps in development
+    optimizeFonts: false, // Disable font optimization
     images: {
         // domains: ['drive.google.com'],
         remotePatterns: [
