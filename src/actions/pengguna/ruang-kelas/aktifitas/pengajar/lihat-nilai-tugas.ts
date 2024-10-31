c3318d28cd0681cfa09c1c7eb9246c52e4b18c4f
Rizky Hady <reazon7@gmail.com>
2024-10-31 14:13:23 +0700

    update

--- src/actions/pengguna/ruang-kelas/aktifitas/pengajar/lihat-nilai-tugas.ts
+++ src/actions/pengguna/ruang-kelas/aktifitas/pengajar/lihat-nilai-tugas.ts
@@ -3,31 +3,41 @@
 import { makeJwtGetRequestAction } from '@/utils/action'
 
 export type DataType = {
-  id?: string | null
-  id_aktifitas?: string
-  id_peserta?: string
-  nama?: string
-  foto?: string
-  nilai?: number | null
-  catatan_pengajar?: string
-  catatan_peserta?: string
-  status_pengumpulan?: boolean
-  waktu_pengumpulan?: string | null
-  berkas?: {
-    id: string
-    id_parent: string | null
-    nama: string
-    deskripsi: string
-    content: string
-    ekstensi: string
-    tipe: 'Dokumen' | 'Audio' | 'Video' | 'Gambar' | 'Folder' | 'Teks' | null
-    url: string
-    ukuran: number
-    id_pengguna: string
-    id_instansi: string | null
-    created_at: string
-    updated_at: string
-  }[]
+  id: string | null
+  id_aktifitas: string
+  id_peserta: string
+  nama: string
+  foto: string
+  nilai: number | null
+  email: string
+  catatan_pengajar: string
+  catatan_peserta: string
+  status_pengumpulan: boolean
+  waktu_pengumpulan: string | null
+  berkas:
+    | {
+        id: string
+        id_parent: string | null
+        nama: string
+        deskripsi: string
+        content: string
+        ekstensi: string
+        tipe:
+          | 'Dokumen'
+          | 'Audio'
+          | 'Video'
+          | 'Gambar'
+          | 'Folder'
+          | 'Teks'
+          | null
+        url: string
+        ukuran: number
+        id_pengguna: string
+        id_instansi: string | null
+        created_at: string
+        updated_at: string
+      }[]
+    | null
 }
 
 export const lihatNilaiTugasAction = async (
