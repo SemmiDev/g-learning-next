c5530a1a64b17deb1760867060b28ae3dfad10b5
Rizky Hady <reazon7@gmail.com>
2024-11-22 16:08:15 +0700

    update

--- src/components/page/pengguna/dashboard/recent-file-item.tsx
+++ src/components/page/pengguna/dashboard/recent-file-item.tsx
@@ -21,68 +21,66 @@ export default function RecentFileItem({ file, onPreview }: FileButtonProps) {
   const isPreviewable = isPreviewableFile(file.link ?? '', file.extension)
 
   return (
-    <div className="flex items-center border-b border-b-gray-100 select-none transition duration-200 py-3 px-4 hover:bg-gray-50/50">
-      <div className="flex flex-1 justify-between items-center space-x-2">
-        <div className="flex space-x-2">
-          <FileIcon file={file} />
-          <div className="flex flex-col">
-            <Text
-              weight="semibold"
-              variant="dark"
-              title={file.name}
-              className="truncate"
-            >
-              {file.name}
-            </Text>
-            <ul className="flex flex-wrap items-center gap-x-1 text-sm text-gray-lighter">
-              <li>
-                {file.size ? (
-                  formatBytes(file.size, 2)
-                ) : (
-                  <Time date={file.time} />
-                )}
-              </li>
-              {!!file.size && (
-                <>
-                  <li>
-                    <GoDotFill size={10} />
-                  </li>
-                  <li>
-                    <Time date={file.time} />
-                  </li>
-                </>
+    <div className="flex justify-between items-center gap-x-2 border-b border-b-gray-100 select-none transition duration-200 py-3 px-4 hover:bg-gray-50/50">
+      <div className="flex space-x-2">
+        <FileIcon file={file} />
+        <div className="flex flex-col">
+          <Text
+            weight="semibold"
+            variant="dark"
+            title={file.name}
+            className="line-clamp-2"
+          >
+            {file.name}
+          </Text>
+          <ul className="flex flex-wrap items-center gap-x-1 text-sm text-gray-lighter">
+            <li>
+              {file.size ? (
+                formatBytes(file.size, 2)
+              ) : (
+                <Time date={file.time} />
               )}
-            </ul>
-          </div>
+            </li>
+            {!!file.size && (
+              <>
+                <li>
+                  <GoDotFill size={10} />
+                </li>
+                <li>
+                  <Time date={file.time} />
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-        <div className="flex space-x-1">
-          {!!file.link && (file.type === 'link' || isPreviewable) && (
-            <LinkOrDiv
-              href={
-                file.type === 'link' && !isPreviewable ? file.link : undefined
-              }
-              target="_blank"
+      </div>
+      <div className="flex space-x-1">
+        {!!file.link && (file.type === 'link' || isPreviewable) && (
+          <LinkOrDiv
+            href={
+              file.type === 'link' && !isPreviewable ? file.link : undefined
+            }
+            target="_blank"
+          >
+            <ActionIcon
+              size="sm"
+              variant="outline-hover-colorful"
+              onClick={(e) => {
+                e.stopPropagation()
+                onPreview && onPreview(file)
+              }}
             >
-              <ActionIcon
-                size="sm"
-                variant="outline-hover-colorful"
-                onClick={(e) => {
-                  e.stopPropagation()
-                  onPreview && onPreview(file)
-                }}
-              >
-                <BsEye />
-              </ActionIcon>
-            </LinkOrDiv>
-          )}
-          {file.type !== 'link' && (
-            <LinkOrDiv href={file.link} target="_blank">
-              <ActionIcon size="sm" variant="outline-hover-colorful">
-                <MdOutlineFileDownload />
-              </ActionIcon>
-            </LinkOrDiv>
-          )}
-        </div>
+              <BsEye />
+            </ActionIcon>
+          </LinkOrDiv>
+        )}
+        {file.type !== 'link' && (
+          <LinkOrDiv href={file.link} target="_blank">
+            <ActionIcon size="sm" variant="outline-hover-colorful">
+              <MdOutlineFileDownload />
+            </ActionIcon>
+          </LinkOrDiv>
+        )}
       </div>
     </div>
   )
