37a3462ab21481a89c735506204b9ffc3c10081d
Rizky Hady <reazon7@gmail.com>
2024-11-22 14:22:56 +0700

    update

--- src/components/page/pengguna/profil/body.tsx
+++ src/components/page/pengguna/profil/body.tsx
@@ -49,103 +49,100 @@ export default function ProfilBody() {
           </Button>
         </div>
         <CardSeparator />
-        <table className="text-sm text-gray-dark m-2">
-          <tbody>
-            <DataRow label="Foto">
-              <div className="inline-block relative">
-                <ActionIconTooltip
-                  tooltip="Ganti Foto"
-                  size="sm"
-                  variant="flat"
-                  color="secondary"
-                  className="absolute top-1.5 right-1.5"
-                  onClick={() => setShowFotoModal(true)}
-                >
-                  <LuCamera />
-                </ActionIconTooltip>
-                <Thumbnail
-                  src={data?.foto}
-                  size={150}
-                  rounded="md"
-                  alt="profil"
-                  avatar={data?.nama}
-                  bordered
-                  priority
-                />
-              </div>
-            </DataRow>
-            <DataRow label="Nama Lengkap" outline>
-              {data?.nama || '-'}
-            </DataRow>
-            <DataRow label="Username" outline>
-              {data?.username || '-'}
-            </DataRow>
-            <DataRow label="NIK" outline>
-              {data?.nik || '-'}
-            </DataRow>
-            <DataRow
-              label="Nama Instansi"
-              outlineClassName="flex-col items-start"
-              outline
-            >
-              {data?.instansi?.length
-                ? data?.instansi?.map((item: string, idx: number) => (
-                    <TextSpan key={idx}>{item}</TextSpan>
-                  ))
-                : '-'}
-            </DataRow>
-            <DataRow label="Sinkron" outline dark>
-              Belum tersinkron
-            </DataRow>
-            <DataRow label="Nomor Kontak" outline>
-              {data?.hp || '-'}
-            </DataRow>
-            <DataRow label="Email" outline>
-              {data?.email?.length
-                ? data?.email?.map((item: Record<string, string>) => (
-                    <TextSpan key={item.id}>{item.email}</TextSpan>
-                  ))
-                : '-'}
-            </DataRow>
-            <DataRow label="Website" outline>
-              {data?.situs_web ? (
-                <Link href={data?.situs_web} target="_blank">
-                  <Button
-                    as="span"
-                    variant="text-colorful"
-                    className="h-auto p-0"
-                  >
-                    {data?.situs_web}
-                  </Button>
-                </Link>
-              ) : (
-                '-'
-              )}
-            </DataRow>
-            <DataRow label="Jenis Kelamin" outline>
-              {data?.jenis_kelamin || '-'}
-            </DataRow>
-            <DataRow label="Bio" outline>
-              <SanitizeHTML html={data?.bio || '-'} />
-            </DataRow>
-            <DataRow label="Kata sandi">
-              <Button
-                variant="outline"
-                color="warning"
-                onClick={() => setShowPasswordModal(true)}
+        <div className="flex flex-col gap-y-4 text-sm text-gray-dark px-2 py-3">
+          <DataRow label="Foto">
+            <div className="inline-block relative">
+              <ActionIconTooltip
+                tooltip="Ganti Foto"
+                size="sm"
+                variant="flat"
+                color="secondary"
+                className="absolute top-1.5 right-1.5"
+                onClick={() => setShowFotoModal(true)}
               >
-                Ubah Kata Sandi
-              </Button>
-            </DataRow>
-          </tbody>
-        </table>
+                <LuCamera />
+              </ActionIconTooltip>
+              <Thumbnail
+                src={data?.foto}
+                size={150}
+                rounded="md"
+                alt="profil"
+                avatar={data?.nama}
+                bordered
+                priority
+              />
+            </div>
+          </DataRow>
+          <DataRow label="Nama Lengkap" outline>
+            {data?.nama || '-'}
+          </DataRow>
+          <DataRow label="Username" outline>
+            {data?.username || '-'}
+          </DataRow>
+          <DataRow label="NIK" outline>
+            {data?.nik || '-'}
+          </DataRow>
+          <DataRow
+            label="Nama Instansi"
+            outlineClassName="flex-col items-start"
+            outline
+          >
+            {data?.instansi?.length
+              ? data?.instansi?.map((item: string, idx: number) => (
+                  <TextSpan key={idx}>{item}</TextSpan>
+                ))
+              : '-'}
+          </DataRow>
+          <DataRow label="Nomor Kontak" outline>
+            {data?.hp || '-'}
+          </DataRow>
+          <DataRow label="Email" outline>
+            {data?.email?.length
+              ? data?.email?.map((item: Record<string, string>) => (
+                  <TextSpan key={item.id}>{item.email}</TextSpan>
+                ))
+              : '-'}
+          </DataRow>
+          <DataRow label="Website" outline>
+            {data?.situs_web ? (
+              <Link href={data?.situs_web} target="_blank">
+                <Button
+                  as="span"
+                  variant="text-colorful"
+                  className="h-auto p-0"
+                >
+                  {data?.situs_web}
+                </Button>
+              </Link>
+            ) : (
+              '-'
+            )}
+          </DataRow>
+          <DataRow label="Jenis Kelamin" outline>
+            {data?.jenis_kelamin || '-'}
+          </DataRow>
+          <DataRow label="Bio" outline>
+            <SanitizeHTML html={data?.bio || '-'} />
+          </DataRow>
+          <DataRow label="Kata sandi">
+            <Button
+              variant="outline"
+              color="warning"
+              onClick={() => setShowPasswordModal(true)}
+            >
+              Ubah Kata Sandi
+            </Button>
+          </DataRow>
+        </div>
       </Card>
 
       <UbahModal showModal={showUbahModal} setShowModal={setShowUbahModal} />
+
       <UbahPasswordModal
         showModal={showPasswordModal}
         setShowModal={setShowPasswordModal}
       />
+
       <UbahFotoModal
         showModal={showFotoModal}
         setShowModal={setShowFotoModal}
@@ -168,9 +165,11 @@ function DataRow({
   outlineClassName?: string
 }) {
   return (
-    <tr className="[&>td]:py-2">
-      <td className="w-40 font-semibold align-baseline">{label}</td>
-      <td>
+    <div className="grid grid-cols-12 gap-y-1">
+      <div className="col-span-12 sm:col-span-3 lg:col-span-2">
+        <TextSpan weight="semibold">{label}</TextSpan>
+      </div>
+      <div className="col-span-12 sm:col-span-9 lg:col-span-10">
         {outline ? (
           <TextBordered
             className={cn({ 'bg-gray-100': dark }, outlineClassName)}
@@ -180,7 +179,7 @@ function DataRow({
         ) : (
           children
         )}
-      </td>
-    </tr>
+      </div>
+    </div>
   )
 }
