27cc59ee19ee3370c55b1fe1c15dfe6cfba76504
Rizky Hady <reazon7@gmail.com>
2024-12-13 15:08:00 +0700

    update

--- src/components/ui/modal/file-preview/video.tsx
+++ src/components/ui/modal/file-preview/video.tsx
@@ -17,7 +17,7 @@ export default function ModalVideoPreview({
       onClose={onClose}
       className="[&>div]:p-0"
       containerClassName="w-fit max-w-full bg-gray-dark/20"
-      bodyClassName="aspect-video w-[100dvw] max-w-[100dvw] max-h-[100dvh] xl:w-[1280px] xl:max-h-[calc(100dvh-2rem)]"
+      bodyClassName="aspect-video w-[100dvw] max-w-[100dvw] max-h-[100dvh] xl:w-[1280px] xl:max-h-[calc(100dvh-3rem)]"
     >
       {!!openUrl && (
         <ReactPlayer url={openUrl} width="100%" height="100%" playing />
