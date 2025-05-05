21d78949cc94e8b421958a3046399b45e0ba385e
Rizky Hady <reazon7@gmail.com>
2025-05-05 09:10:04 +0700

    delete codes used for slicing

--- src/components/page/dashboard.tsx
+++ src/components/page/dashboard.tsx
@@ -2,9 +2,7 @@
 
 import DashboardAdminBody from '@/components/page/admin/dashboard/body'
 import DashboardInstansiBody from '@/components/page/instansi/dashboard/body'
-import DashboardPengajarBody from '@/components/page/pengajar/dashboard/body'
 import DashboardPenggunaBody from '@/components/page/pengguna/dashboard/body'
-import DashboardPesertaBody from '@/components/page/peserta/dashboard/body'
 import { useSessionPengguna } from '@/hooks/use-session-pengguna'
 
 export default function DashboardBody() {
@@ -15,10 +13,6 @@ export default function DashboardBody() {
       return <DashboardAdminBody />
     case 'Instansi':
       return <DashboardInstansiBody />
-    case 'Pengajar':
-      return <DashboardPengajarBody />
-    case 'Peserta':
-      return <DashboardPesertaBody />
     case 'Pengguna':
       return <DashboardPenggunaBody />
     default:
