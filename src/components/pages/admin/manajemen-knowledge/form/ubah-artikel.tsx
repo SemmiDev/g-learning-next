e588e17101243c8c1cadec9d3e33cb14c88d131e
Rizky Hady <reazon7@gmail.com>
2025-08-29 09:44:35 +0700

    review artikel knowledge

--- src/components/pages/admin/manajemen-knowledge/form/ubah-artikel.tsx
+++ src/components/pages/admin/manajemen-knowledge/form/ubah-artikel.tsx
@@ -1,5 +1,6 @@
 import {
   ActionIcon,
+  Button,
   ButtonSubmit,
   ContentLoader,
   ControlledInput,
@@ -8,6 +9,7 @@ import {
   Form,
   FormError,
   Loader,
+  Modal,
   SelectOptionType,
   Title,
 } from '@/components/ui'
@@ -38,6 +40,7 @@ export type UbahArtikelFormSchema = {
   level?: SelectOptionType
   isi?: string
   modul?: string
+  slug?: string
 }
 
 const levelOptions: SelectOptionType[] = [
@@ -53,12 +56,14 @@ export default function UbahArtikelForm() {
   const { updateArtikelItem } = useManajemenKnowledgeSortableStore()
   const { id, tutupArtikel } = useManajemenKnowledgeArtikelStore()
 
+  const [showReview, setShowReview] = useState(false)
+
   const [formError, setFormError] = useState<string>()
 
   const queryKey = ['admin.manajemen-knowledge.artikel.ubah', id]
 
   const {
-    data: initialValues,
+    data: initialValues = {},
     isLoading,
     isFetching,
   } = useQuery<UbahArtikelFormSchema>({
@@ -73,11 +78,12 @@ export default function UbahArtikelForm() {
         level: levelOptions.find((item) => item.value === data?.level),
         isi: data?.isi,
         modul: data?.id_modul,
+        slug: data?.slug,
       }
     },
   })
 
-  const idModul = initialValues?.modul
+  const { modul: idModul, slug } = initialValues
 
   const {
     data: dataModul,
@@ -112,91 +118,114 @@ export default function UbahArtikelForm() {
 
   if (isLoading) return <ContentLoader height={460} />
 
+  if (!id) return null
+
   return (
-    <Form<UbahArtikelFormSchema>
-      onSubmit={onSubmit}
-      validationSchema={formSchema}
-      useFormProps={{
-        mode: 'onSubmit',
-        values: initialValues,
-        defaultValues: initialValues,
-      }}
-      className="flex flex-col gap-4"
-    >
-      {({ control, formState: { errors, isSubmitting, isDirty } }) => (
-        <>
-          <div className="flex items-center justify-between gap-2">
-            <div className="flex gap-0.5 flex-wrap">
-              {isLoadingModul ? (
-                <Loader size="sm" color="primary" />
-              ) : (
-                <Title as="h4" size="base" weight="semibold">
-                  {dataModul?.nama || '-'}
-                </Title>
-              )}
-              {(isFetching || isFetchingModul) && (
-                <Loader size="sm" variant="pulse" />
-              )}
+    <>
+      <Form<UbahArtikelFormSchema>
+        onSubmit={onSubmit}
+        validationSchema={formSchema}
+        useFormProps={{
+          mode: 'onSubmit',
+          values: initialValues,
+          defaultValues: initialValues,
+        }}
+        className="flex flex-col gap-4"
+      >
+        {({ control, formState: { errors, isSubmitting, isDirty } }) => (
+          <>
+            <div className="flex items-center justify-between gap-2 flex-wrap-reverse">
+              <div className="flex gap-0.5 flex-wrap">
+                {isLoadingModul ? (
+                  <Loader size="sm" color="primary" />
+                ) : (
+                  <Title as="h4" size="base" weight="semibold">
+                    {dataModul?.nama || '-'}
+                  </Title>
+                )}
+                {(isFetching || isFetchingModul) && (
+                  <Loader size="sm" variant="pulse" />
+                )}
+              </div>
+              <div className="flex justify-end gap-2 flex-grow">
+                <Button
+                  color="info"
+                  disabled={isDirty}
+                  onClick={() => setShowReview(true)}
+                >
+                  Review
+                </Button>
+                <ButtonSubmit
+                  color="warning"
+                  isSubmitting={isSubmitting}
+                  disabled={!isDirty}
+                  className="sm:px-4 sm:py-2 sm:text-sm sm:min-h-10"
+                >
+                  Simpan
+                </ButtonSubmit>
+                <ActionIcon
+                  variant="text"
+                  color="gray"
+                  onClick={tutupArtikel}
+                  className="text-gray-lighter sm:p-1 sm:w-9 sm:h-9"
+                >
+                  <MdClose className="size-4" />
+                </ActionIcon>
+              </div>
             </div>
-            <div className="flex gap-2">
-              <ButtonSubmit
-                color="warning"
-                isSubmitting={isSubmitting}
-                disabled={!isDirty}
-                className="sm:px-4 sm:py-2 sm:text-sm sm:min-h-10"
-              >
-                Simpan
-              </ButtonSubmit>
-              <ActionIcon
-                variant="text"
-                color="gray"
-                onClick={tutupArtikel}
-                className="text-gray-lighter sm:p-1 sm:w-9 sm:h-9"
-              >
-                <MdClose className="size-4" />
-              </ActionIcon>
+
+            <FormError error={formError} />
+
+            <div className="flex gap-2 flex-wrap">
+              <ControlledInput
+                name="judul"
+                control={control}
+                errors={errors}
+                label="Judul Artikel"
+                placeholder="Tulis judul artikel di sini"
+                className="flex-1"
+                required
+              />
+
+              <ControlledSelect
+                name="level"
+                control={control}
+                options={levelOptions}
+                label="Level"
+                placeholder="Pilih Level"
+                errors={errors}
+                className="w-full sm:w-36"
+                required
+              />
             </div>
-          </div>
 
-          <FormError error={formError} />
-
-          <div className="flex gap-2 flex-wrap">
-            <ControlledInput
-              name="judul"
+            <ControlledQuillEditor
+              name="isi"
               control={control}
               errors={errors}
-              label="Judul Artikel"
-              placeholder="Tulis judul artikel di sini"
-              className="flex-1"
+              label="Isi Artikel"
+              placeholder="Tulis isi artikel di sini"
+              size="xl"
+              toolbar="rich"
+              className="text-gray-dark"
+              toolbarImage
               required
             />
+          </>
+        )}
+      </Form>
 
-            <ControlledSelect
-              name="level"
-              control={control}
-              options={levelOptions}
-              label="Level"
-              placeholder="Pilih Level"
-              errors={errors}
-              className="w-full sm:w-36"
-              required
-            />
-          </div>
-
-          <ControlledQuillEditor
-            name="isi"
-            control={control}
-            errors={errors}
-            label="Isi Artikel"
-            placeholder="Tulis isi artikel di sini"
-            size="xl"
-            toolbar="rich"
-            className="text-gray-dark"
-            toolbarImage
-            required
-          />
-        </>
-      )}
-    </Form>
+      <Modal
+        size="full"
+        title="Review Artikel"
+        isOpen={showReview}
+        onClose={() => setShowReview(false)}
+      >
+        <iframe
+          src={`${process.env.NEXT_PUBLIC_KNOWLEDGE_URL}/artikel/${slug}?review=true`}
+          className="flex-1"
+        />
+      </Modal>
+    </>
   )
 }
